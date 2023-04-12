b244ae196997914746cd88b3b297aa25cde3fcff
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-04-12 23:48:56 +0300

    semi

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
 import throttle from "lodash.throttle";
+import player from "@vimeo/player";
+
+    const STORAGE_KEY = "videoplayer-current-time";
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe); 
+
+    function getLocalStorage (data){
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds))};
+
+    player.on('timeupdate', throttle(getLocalStorage,1000));
+
+    
+
+    
+
+    
